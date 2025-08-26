a8b09aff6146b9bdd4a478635e5aa4a73968c9e5
hadi <hazard7546@gmail.com>
2025-08-26 10:58:19 +0330

    feat: public/, blog/ and script.js created.

---
+++ script.js
@@ -0,0 +1,36 @@
+async function fetchLastTrack() {
+  try {
+    const response = await fetch(
+      "https://lastfm-last-played.biancarosa.com.br/hadi7546/latest-song",
+    );
+    const data = await response.json();
+    const track = data.track;
+
+    const trackElement = document.getElementById("lastfm-track");
+
+    const image =
+      track.image.find((img) => img.size === "extralarge") ||
+      track.image.find((img) => img.size === "large") ||
+      track.image[track.image.length - 1];
+
+    trackElement.innerHTML = `
+            <div class="track-info">
+                <img src="${image["#text"]}" alt="Album cover" class="album-cover" onerror="this.style.display='none'">
+                <div class="track-details">
+                    <a class="track-name" href="${track.url}" target="_blank">${track.name}</a>
+                    <div class="artist-name">by ${track.artist["#text"]}</div>
+                    <div class="album-name">from ${track.album["#text"]}</div>
+                    <div class="play-date">${track.date["#text"]}</div>
+                </div>
+            </div>
+        `;
+  } catch (error) {
+    console.error("Error fetching Last.fm data:", error);
+    document.getElementById("lastfm-track").innerHTML =
+      '<div class="error">Unable to load track information</div>';
+  }
+}
+
+document.addEventListener("DOMContentLoaded", fetchLastTrack);
+
+setInterval(fetchLastTrack, 5 * 60 * 1000);
